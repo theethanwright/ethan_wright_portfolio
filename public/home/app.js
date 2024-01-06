a222708055f06d18a46223e568357b29ba17a5f6
theethanwright <theethanwright@gmail.com>
2024-01-06 12:29:13 -0500

    All base elements complete

--- public/home/app.js
+++ public/home/app.js
@@ -1,12 +1,3 @@
-function myFunction() {
-  var x = document.getElementById("myTopnav");
-  if (x.className === "topnav") {
-    x.className += " responsive";
-  } else {
-    x.className = "topnav";
-  }
-}
-
 const canvasSize = {
     width: window.innerWidth,
     height: window.innerHeight
