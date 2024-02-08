ea84e7d8cf661eab726cd881a5824beba10939eb
theethanwright <theethanwright@gmail.com>
2024-02-08 01:31:47 -0500

    image transition

--- public/work/public/app.js
+++ public/work/public/app.js
@@ -1,24 +1,24 @@
-const router = useRouter();
+var dots = document.querySelectorAll(".preview_img_link");
 
-const [transitionStarted, setTransitionStarted] = useState<boolean>(false);
+for(var i = 0; i < dots.length; i ++){
+dots[i].dotIndex = i;
+dots[i].addEventListener("click", function(){
+  var a = this.dotIndex;
+  console.log(this.dotIndex);
+  document.getElementById(a).classList.remove("overflow-hidden");
+  document.getElementById(a).style.display = `fixed`;
+  document.getElementById(a).style.width = `1vw`;
+  document.getElementById(a).style.height = `1vw`;
+  var imgElements = document.getElementsByTagName('img');
 
-function handleClick() {
-  setTransitionStarted(true);
-
-  setTimeout(() => {
-    router.push("/next-page");
-  }, 500)
+// Loop through each img element
+for (var i = 0; i < imgElements.length; i++) {
+    // Remove the class from each img element
+    imgElements[i].classList.remove("hover:scale-125");
+}
+})
 }
 
-return (
-  <div>
-    <Link className="hidden" href="/next-page" />
-    <Image 
-      className={`duration-500 ${transitionStarted ? "fixed w-full h-full top-0 left-0" : "<initial image sizing and styles>"}`} 
-      onClick={handleClick}> 
-      src="path-to-image" 
-      fill 
-      alt="image link" />
-  </div>
-  
-)
+function delay (URL) {
+  setTimeout( function() { window.location = URL }, 10000 );
+}
